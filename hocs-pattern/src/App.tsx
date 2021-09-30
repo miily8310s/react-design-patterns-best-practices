71d20d4614326048b051025a10bc98c7497d31f3
miruoo <42486288+miily8310s@users.noreply.github.com>
2021-09-30 21:45:09 +0900

    Feat/container presentational pattern (#1)
    * Add Learning memo
    
    * feat: container-pattern Add geolocation app
    
    * feat:container-presentation Initialize hock app
    
    * feat:container-presentation Add hock app

--- hocs-pattern/src/App.tsx
+++ hocs-pattern/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import GeolocationContainer from "./components/GeolocationContainer";
+import MyComponent from "./components/MyComponent";
 
 function App() {
   return (
     <div className="App">
-      <GeolocationContainer />
+      <MyComponent />
     </div>
   );
 }
